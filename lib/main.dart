431e33afc6dcb784751e1ac2b3c15a10524905d8
JV Lands PVT <jvlandspvt@JVs-MacBook-Pro.local>
2023-03-23 12:09:19 +0500

    Chapter 4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_app/screens/homePage.dart';
 import 'package:flutter_app/screens/loginPage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -13,19 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       themeMode: ThemeMode.light,
       theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily,
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/",
       routes: {
-        "/":(context)=> LoginPage(),
-        "/home":(context)=> HomePage(),
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
       },
     );
   }
--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -6,56 +6,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20.0,
-          ),
-          Image.asset("assets/images/login_image.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            "Welcome to My Catalog",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+        color: Colors.white,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter User name",
-                    labelText: "User Name",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(onPressed: (){
-                  print("Welcome to My Catalog");
-                },
-                    style: TextButton.styleFrom(),
-                    child: Text("Login"))
-              ],
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
             ),
-          ),
-
-        ],
-      )
-    );
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Welcome to My Catalog",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter User name",
+                      labelText: "User Name",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Welcome to My Catalog");
+                      },
+                      style: TextButton.styleFrom(),
+                      child: Text("Login"))
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
